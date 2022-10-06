631c47e44e4b828e32c803c15cae559479b0db94
HP <HP@WIN-8VMT11N5JJN>
2022-10-06 19:06:34 +0300

    Добавьте файлы проекта.

---
+++ p5.3/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите расстояние в см :"); // вывод на экран строки "..."
+        int i = int.Parse(Console.ReadLine()); // ввод i
+        Console.Write("Полных м: "); // вывод на экран строки "..."
+        Console.WriteLine(i / 100); // вывод на экран строки "..."
+    }
+}
